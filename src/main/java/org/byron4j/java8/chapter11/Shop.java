b2a77a6819303cdc9b4d1f2de593384bb75b4ce4
byron4j <byron4j@126.com>
2022-02-26 13:45:24 +0800

    【comment】异步化 【author】边缘人onj

--- src/main/java/org/byron4j/java8/chapter11/Shop.java
+++ src/main/java/org/byron4j/java8/chapter11/Shop.java
@@ -1,6 +1,9 @@
 package org.byron4j.java8.chapter11;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * 在线商店
@@ -30,5 +33,46 @@ public class Shop {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 异步化
+     *//*
+    public Future<Double> getPriceAsync(String product){
+        CompletableFuture<Double> future
+                = new CompletableFuture<>();
+        // 耗时操作新线程操作
+        new Thread(
+                () -> {
+                    double v = calculatorPrice(product);
+                    int i = 1 / 0;
+                    future.complete(v);
+                }
+        ).start();
+        return future;
+    }*/
+
+    /**
+     * 异步化
+     */
+    public Future<Double> getPriceAsync(String product){
+        // 使用静态工厂方法创建 CompletableFuture
+        CompletableFuture<Double> future =
+                CompletableFuture.supplyAsync(() -> calculatorPrice(product));
+        return future;
+    }
+
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        Future<Double> cookie = shop.getPriceAsync("Cookie");
+        Double aDouble = null;
+        try {
+            aDouble = cookie.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(aDouble);
     }
 }
